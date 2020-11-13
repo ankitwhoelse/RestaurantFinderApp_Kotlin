d46208f9d05f07b3849af1fb21b6f94c416e5fc1
ankitwhoelse <ankitvinod@gmail.com>
2020-11-13 16:28:33 -0500

    map part1

--- app/src/main/java/ca/qc/cgodin/projetfinal/MainActivity.kt
+++ app/src/main/java/ca/qc/cgodin/projetfinal/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
