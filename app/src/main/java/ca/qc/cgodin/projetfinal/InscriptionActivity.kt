1c72ae3e06ccf642e32f777a24b76f86747e090d
Ribensky Nader Merzius <RN.Merzius@cgodin.qc.ca>
2020-11-13 16:28:16 -0500

    login ui

---
+++ app/src/main/java/ca/qc/cgodin/projetfinal/InscriptionActivity.kt
@@ -0,0 +1,11 @@
+package ca.qc.cgodin.projetfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InscriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription)
+    }
+}
