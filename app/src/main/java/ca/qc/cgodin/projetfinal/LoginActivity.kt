7f296bc917d02d0f36957997cf8f18df371bc321
ankitwhoelse <ankitvinod@gmail.com>
2020-11-13 14:30:49 -0500

    Navigation complete

--- app/src/main/java/ca/qc/cgodin/projetfinal/LoginActivity.kt
+++ app/src/main/java/ca/qc/cgodin/projetfinal/LoginActivity.kt
@@ -2,10 +2,15 @@ package ca.qc.cgodin.projetfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
     }
 }
