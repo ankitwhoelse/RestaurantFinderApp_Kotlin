936810ac3e17a75fc4469049c8d12585dc338312
Ribensky Nader Merzius <RN.Merzius@cgodin.qc.ca>
2020-11-17 16:33:47 -0500

    Classe du api restaurant ajouté

---
+++ app/src/main/java/ca/qc/cgodin/projetfinal/models/Location.kt
@@ -0,0 +1,6 @@
+package ca.qc.cgodin.projetfinal.models
+
+data class Location(
+    val lat: Double,
+    val lng: Double
+)
---
+++ app/src/main/java/ca/qc/cgodin/projetfinal/models/Viewport.kt
@@ -0,0 +1,6 @@
+package ca.qc.cgodin.projetfinal.models
+
+data class Viewport(
+    val northeast: Northeast,
+    val southwest: Southwest
+)
